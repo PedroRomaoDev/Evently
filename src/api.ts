094db9feece75e2c7aa58ebd008c333cb368c1b9
PedroRomaoDev <pedro7ntj@gmail.com>
2025-11-25 19:55:30 -0300

    feat: zod validator to fastify

--- src/api.ts
+++ src/api.ts
@@ -1,4 +1,10 @@
-import fastify, { FastifyReply, FastifyRequest } from 'fastify';
+import fastify from 'fastify';
+import type { ZodTypeProvider } from 'fastify-type-provider-zod';
+import {
+  serializerCompiler,
+  validatorCompiler,
+} from 'fastify-type-provider-zod';
+import z from 'zod';
 
 import { CreateEvent } from './application/CreateEvent.js';
 import { db } from './db/client.js';
@@ -6,34 +12,59 @@ import { EventRepositoryDrizzle } from './resources/EventRepository.js';
 
 const app = fastify();
 
-app.post('/events', async (req: FastifyRequest, res: FastifyReply) => {
-  const { name, ticketPriceInCents, latitude, longitude, ownerId, date } =
-    req.body as {
-      name: string;
-      ticketPriceInCents: number;
-      latitude: number;
-      longitude: number;
-      ownerId: string;
-      date: string;
-    };
-  try {
-    const eventRepositoryDrizzle = new EventRepositoryDrizzle(db);
-    const createEvent = new CreateEvent(eventRepositoryDrizzle);
-    const event = await createEvent.execute({
-      ownerId,
-      name,
-      ticketPriceInCents,
-      latitude,
-      longitude,
-      date: new Date(date),
-    });
-    return res.status(201).send(event);
-  } catch (error) {
-    if (error instanceof Error) {
+app.setValidatorCompiler(validatorCompiler);
+app.setSerializerCompiler(serializerCompiler);
+
+app.withTypeProvider<ZodTypeProvider>().route({
+  method: 'POST',
+  url: '/events',
+  schema: {
+    body: z.object({
+      name: z.string(),
+      ticketPriceInCents: z.number(),
+      longitude: z.number(),
+      latitude: z.number(),
+      ownerId: z.uuid(),
+      date: z.iso.datetime(),
+    }),
+    response: {
+      201: z.object({
+        id: z.uuid(),
+        name: z.string(),
+        ticketPriceInCents: z.number(),
+        longitude: z.number(),
+        latitude: z.number(),
+        ownerId: z.uuid(),
+        date: z.iso.datetime(),
+      }),
+      400: z.object({
+        message: z.string(),
+      }),
+    },
+  },
+  handler: async (req, res) => {
+    const { name, ticketPriceInCents, latitude, longitude, ownerId, date } =
+      req.body;
+    try {
+      const eventRepositoryDrizzle = new EventRepositoryDrizzle(db);
+      const createEvent = new CreateEvent(eventRepositoryDrizzle);
+      const event = await createEvent.execute({
+        ownerId,
+        name,
+        ticketPriceInCents,
+        latitude,
+        longitude,
+        date: new Date(date),
+      });
+      return res.status(201).send({
+        ...event,
+        date: event.date.toISOString(),
+      });
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    } catch (error: any) {
       return res.status(400).send({ message: error.message });
     }
-    return res.status(400).send({ message: 'Erro desconhecido' });
-  }
+  },
 });
 
 app.listen({ port: 3000 }, () => {
