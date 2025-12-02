bfbf983bc64171af23fe046154632a3009690f17
PedroRomaoDev <pedro7ntj@gmail.com>
2025-12-02 13:13:50 -0300

    refactor: add Input and Output DTO interfaces to GetEvent use case

--- src/application/GetEvent.ts
+++ src/application/GetEvent.ts
@@ -1,15 +1,31 @@
 import { OnSiteEvent } from './entities/OnSiteEvent.js';
 import { InvalidEventIdError, NotFoundError } from './errors/index.js';
 
+// DTO - Data Transfer Object
+interface Input {
+  eventId: string;
+}
+
+// DTO - Data Transfer Object
+interface Output {
+  id: string;
+  ownerId: string;
+  name: string;
+  ticketPriceInCents: number;
+  date: Date;
+  latitude: number;
+  longitude: number;
+}
+
 export interface GetEventRepository {
   getById: (id: string) => Promise<OnSiteEvent | null>;
 }
 
 export class GetEvent {
-  constructor(private repository: GetEventRepository) {}
+  constructor(private eventRepository: GetEventRepository) {}
 
-  async execute(eventId: string) {
-    // valeventIdate eventId is uuid
+  async execute(input: Input): Promise<Output> {
+    const { eventId } = input;
     if (
       !/^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}$/.test(
         eventId
@@ -18,7 +34,7 @@ export class GetEvent {
       throw new InvalidEventIdError();
     }
 
-    const event = await this.repository.getById(eventId);
+    const event = await this.eventRepository.getById(eventId);
     if (!event) {
       throw new NotFoundError();
     }
